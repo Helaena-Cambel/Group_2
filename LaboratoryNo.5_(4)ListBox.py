92fc9b49d3fb4437ba27e1c09cdf2c23ad4e5025
Helaena-Cambel <98925403+Helaena-Cambel@users.noreply.github.com>
2022-04-12 21:44:01 +0800

    Add files via upload

---
+++ LaboratoryNo.5_(4)ListBox.py
@@ -0,0 +1,18 @@
+#Frances Helaena Cambel from Group 2 (OOP 58001)
+
+from tkinter import*
+from tkinter import ttk
+window = Tk()
+
+# Window
+window.geometry("300x100")
+window.title("Major Subjects")
+
+# List Box
+data = "reading", "writing", "arithmetic", "coding"
+lb = Listbox(window, height = 5, selectmode = "single")
+for num in data:
+    lb.insert(END, num)
+lb.place(x=85, y=10)
+
+window.mainloop()
